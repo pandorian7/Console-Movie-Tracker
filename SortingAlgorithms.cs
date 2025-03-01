825a06a8f804ae4027e70e1529d972cf2dc4a17a
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2025-03-01 20:42:00 +0530

    Centralized Sotring Algorithms

--- SortingAlgorithms.cs
+++ SortingAlgorithms.cs
@@ -1,5 +1,4 @@
 using System;
-using MovieTracker;
 
 namespace DSA;
 
@@ -39,6 +38,26 @@ public class SelectionSort
             T1 temp = arr.At(i);
             arr.Set(i, arr.At(minIndex));
             arr.Set(minIndex, temp);
+        }
+    }
+}
+
+public class BubbleSort {
+    public static void Sort<T1, T2>(DynamicArray<T1> arr, Func<T1, T2> key) where T2 : IComparable<T2> {
+        int n = arr.Count;
+        bool swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (key(arr.At(j)).CompareTo(key(arr.At(j+1))) > 0) {
+                    T1 temp = arr.At(j);
+                    arr.Set(j,arr.At(j + 1));
+                    arr.Set(j + 1, temp);
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
         }
     }
 }
